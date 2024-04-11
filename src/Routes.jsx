c5d846200a3ca672ca0c4849e2f79819aebb5977
123ZOKzok <zippyondiba@gmail.com>
2024-04-11 20:56:53 +0300

    Routing bug fixed

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Products from './components/Products';
 import Settings from './components/Settings';
@@ -6,14 +6,15 @@ import Settings from './components/Settings';
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
+      <Switch>
+        <Route path="/" exact component={Dashboard} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/products" component={Products} />
+        <Route path="/settings" component={Settings} />
+      </Switch>
     </Router>
   );
 }
+
 
 
